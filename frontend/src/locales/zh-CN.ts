5678019da49faf5a09072a3131e012416b021b8f
leowu <wupengcn301@gmail.com>
2023-04-02 14:44:20 +0800

    增加中文国际化支持

--- frontend/src/locales/zh-CN.ts
+++ frontend/src/locales/zh-CN.ts
@@ -1,10 +1,6 @@
-import component from './zh-CN/component';
-import globalHeader from './zh-CN/globalHeader';
 import menu from './zh-CN/menu';
 import pages from './zh-CN/pages';
-import pwa from './zh-CN/pwa';
 import settingDrawer from './zh-CN/settingDrawer';
-import settings from './zh-CN/settings';
 
 export default {
   'navBar.lang': '语言',
@@ -16,10 +12,6 @@ export default {
   'app.welcome.link.fetch-blocks': '获取全部区块',
   'app.welcome.link.block-list': '基于 block 开发，快速构建标准页面',
   ...pages,
-  ...globalHeader,
   ...menu,
   ...settingDrawer,
-  ...settings,
-  ...pwa,
-  ...component,
 };
